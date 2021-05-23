9126ed7864f840dd330319759f4b090090abbaff
Rolando <rolovargas@gmail.com>
2021-05-23 11:28:10 -0700

    include the question number in the questions list page

--- src/app/question.ts
+++ src/app/question.ts
@@ -1,10 +1,12 @@
 export class Question {
+  questionNumber = 0;
   category = '';
   subCategory = '';
   question = '';
   answer = '';
 
-  constructor(category, subCategory, question, answer) {
+  constructor(questionNumber, category, subCategory, question, answer) {
+    this.questionNumber = questionNumber;
     this.category = category;
     this.subCategory = subCategory;
     this.question = question;
